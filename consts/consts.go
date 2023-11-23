766a7cfbf3943d82b26b06b683500afb254f2ba3
Kylebaltazar <60884821+Kylebaltazar@users.noreply.github.com>
2023-11-23 13:32:15 +0800

    AVA-HYPERSDK

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "kyle"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "axie"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "axs"
 )
 
 var ID ids.ID
